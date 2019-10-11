8058b7715aa3b9d686aefed9588449802ef84b05
Helio Franca <helio.franca@rsinet.com.br>
2019-10-11 17:29:07 -0300

    Tab Navigator

--- React-Native/ProjetoMobile/App.js
+++ React-Native/ProjetoMobile/App.js
@@ -1,35 +1,39 @@
-import React, { Component } from "react"
-import {  createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-import Tela1 from './src/screens/tela1';
-import Tela2 from './src/screens/tela2';
+class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Home!</Text>
+            </View>
+        );
+    }
+}
 
-const AppNavigator = createStackNavigator({
-    Tela1: {
-        screen: Tela1
-    },
-    Tela2: {
-        screen: Tela2,
-            /** Configuração por tela */
-        navigationOptions:{
-            headerTintColor:'pink',
-            headerTitleStyle:{
-                fontSize:20
+class SettingsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Settings!</Text>
+            </View>
+        );
+    }
+}
+
+const TabNavigator = createBottomTabNavigator({
+    Home: HomeScreen,
+    Settings: SettingsScreen,
+}, {
+    defaultNavigationOptions: {
+        tabBarOptions:{
+            labelStyle:{
+                fontSize:22
             }
-        }
-    },
-},{
-    /** Configuração padrão */
-    defaultNavigationOptions:{
-        headerStyle:{
-            backgroundColor:'blue'
-        },
-        headerTintColor:'white',
-        headerTitleStyle:{
-            fontWeight:'bold',
-            fontSize:20
         }
     }
 });
-export default createAppContainer(AppNavigator);
+
+export default createAppContainer(TabNavigator);
