20cf89fb0e2fe19c4cbad3b837cbe49f32721783
Helio Franca <helio.franca@rsinet.com.br>
2019-12-02 19:39:09 -0300

    Upload de Foto no FireBase

--- React-Native/SegundoProjetoMobile/App.js
+++ React-Native/SegundoProjetoMobile/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { View, Text, Button, Image } from "react-native";
 import imagePicker from "react-native-image-picker";
+import RNFetchBlob from 'react-native-fetch-blob'
+import firebase from './src/config/firebaseConfig'
+
+window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
+window.Blob = RNFetchBlob.polyfill.Blob;
 
 export default class App extends Component {
   constructor(props) {
@@ -13,15 +18,27 @@ export default class App extends Component {
 
   pegarFoto() {
 
-    let options={
-      title:"Selecionar Foto"
+    let options = {
+      title: "Selecionar Foto"
     }
     imagePicker.showImagePicker(options, (resposta) => {
       if (resposta.uri) {
-        let foto = {uri:resposta.uri};
-        let state = this.state;
-        state.foto=foto;
-        this.setState(state)
+        let imagem = firebase.acessarPastaAbaixo('imagem.jpg');
+        let mime = 'image/jpeg'
+        RNFetchBlob.fs.readFile(resposta.uri, 'base64')
+          .then((data) => {
+            return RNFetchBlob.polyfill.Blob.build(data, { type: mime + ';BASE64' });
+          }).then((blob) => {
+            imagem.put(blob, { contentType: mime })
+              .then(() => {
+                blob.close();
+                alert("Terminou o Processo!");
+              }).catch((error) => {
+                alert(error.code);
+              })
+          });
+
+
       }
     })
   }
@@ -31,7 +48,7 @@ export default class App extends Component {
       <View>
         <Text style={{ fontSize: 26, marginBottom: 10 }}>App</Text>
         <Button title="Carregar Foto " onPress={this.pegarFoto} />
-        <Image source={this.state.foto} style={{height:90, width:90}}></Image>
+        <Image source={this.state.foto} style={{ height: 90, width: 90 }}></Image>
       </View>
     );
   }
